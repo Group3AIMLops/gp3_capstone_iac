385ddd7249f1a389d6ea9d88d093bba66e21a59f
durgeshshisode1988 <durgesh.shisode@gmail.com>
2024-07-14 12:28:52 +0530

    Create Infra

--- iac_files/backend.tf
+++ iac_files/backend.tf
@@ -2,7 +2,7 @@ terraform{
     backend "s3" {
         bucket = "gp3-terraform-state-bucket-1"
         key = "prod/terraform.tfstate"
-        region = "us-east-2"
+        region = "us-east-1"
         dynamodb_table = "gp3-terraform-locks"
         encrypt        = true
     }
